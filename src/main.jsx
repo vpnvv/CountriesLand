264ad70794397dd57d4ac9c3ce7d11719abe58a0
vpnvv <vipinvv555@gmail.com>
2025-06-01 04:11:36 +0530

    fixed baseName issue on route issues

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/CountriesLand">
         <App />
       </BrowserRouter>
     </Provider>
